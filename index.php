75126ddb3951599c1a72887a1cf17efee3ab65b2
BaoBin <baobin81@qq.com>
2018-08-24 14:11:03 +0800

    测试

--- index.php
+++ index.php
@@ -11,47 +11,47 @@
  * @copyright 2009 Lucky
  * @copyright 2016 Jan-Otto Kröpke <slaver7@gmail.com>
  * @licence MIT
- * @version 1.8.0
- * @link https://github.com/jkroepke/2Moons
- */
+//  * @version 1.8.0
+//  * @link https://github.com/jkroepke/2Moons
+//  */
 
-define('MODE', 'LOGIN');
-define('ROOT_PATH', str_replace('\\', '/',dirname(__FILE__)).'/');
-set_include_path(ROOT_PATH);
+// define('MODE', 'LOGIN');
+// define('ROOT_PATH', str_replace('\\', '/',dirname(__FILE__)).'/');
+// set_include_path(ROOT_PATH);
 
-require 'includes/pages/login/AbstractLoginPage.class.php';
-require 'includes/pages/login/ShowErrorPage.class.php';
-require 'includes/common.php';
-/** @var $LNG Language */
+// require 'includes/pages/login/AbstractLoginPage.class.php';
+// require 'includes/pages/login/ShowErrorPage.class.php';
+// require 'includes/common.php';
+// /** @var $LNG Language */
 
-$page 		= HTTP::_GP('page', 'index');
-$mode 		= HTTP::_GP('mode', 'show');
-$page		= str_replace(array('_', '\\', '/', '.', "\0"), '', $page);
-$pageClass	= 'Show'.ucfirst($page).'Page';
+// $page 		= HTTP::_GP('page', 'index');
+// $mode 		= HTTP::_GP('mode', 'show');
+// $page		= str_replace(array('_', '\\', '/', '.', "\0"), '', $page);
+// $pageClass	= 'Show'.ucfirst($page).'Page';
 
-$path		= 'includes/pages/login/'.$pageClass.'.class.php';
+// $path		= 'includes/pages/login/'.$pageClass.'.class.php';
 
-if(!file_exists($path)) {
-	ShowErrorPage::printError($LNG['page_doesnt_exist']);
-}
+// if(!file_exists($path)) {
+// 	ShowErrorPage::printError($LNG['page_doesnt_exist']);
+// }
 
-// Added Autoload in feature Versions
-require($path);
+// // Added Autoload in feature Versions
+// require($path);
 
-$pageObj	= new $pageClass;
-// PHP 5.2 FIX
-// can't use $pageObj::$requireModule
-$pageProps	= get_class_vars(get_class($pageObj));
+// $pageObj	= new $pageClass;
+// // PHP 5.2 FIX
+// // can't use $pageObj::$requireModule
+// $pageProps	= get_class_vars(get_class($pageObj));
 
-if(isset($pageProps['requireModule']) && $pageProps['requireModule'] !== 0 && !isModuleAvailable($pageProps['requireModule'])) {
-	ShowErrorPage::printError($LNG['sys_module_inactive']);
-}
+// if(isset($pageProps['requireModule']) && $pageProps['requireModule'] !== 0 && !isModuleAvailable($pageProps['requireModule'])) {
+// 	ShowErrorPage::printError($LNG['sys_module_inactive']);
+// }
 
-if(!is_callable(array($pageObj, $mode))) {	
-	if(!isset($pageProps['defaultController']) || !is_callable(array($pageObj, $pageProps['defaultController']))) {
-		ShowErrorPage::printError($LNG['page_doesnt_exist']);
-	}
-	$mode	= $pageProps['defaultController'];
-}
+// if(!is_callable(array($pageObj, $mode))) {	
+// 	if(!isset($pageProps['defaultController']) || !is_callable(array($pageObj, $pageProps['defaultController']))) {
+// 		ShowErrorPage::printError($LNG['page_doesnt_exist']);
+// 	}
+// 	$mode	= $pageProps['defaultController'];
+// }
 
-$pageObj->{$mode}();
+// $pageObj->{$mode}();
